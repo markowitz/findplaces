2971f075bb3c13880ebf778471b9dca0992eea84
Ikechukwu Nwakanma <nwakanma.ikechukwu@yahoo.com>
2018-11-17 23:44:51 +0100

    modify homepage

--- resources/views/home/landing.blade.php
+++ resources/views/home/landing.blade.php
@@ -6,10 +6,111 @@
             <h1>FIND NICE SPOTS AROUND YOU</h1>
             <input type="text" name="" placeholder="Resturants, Bars, Location" class="form-control custom" id=""><button class="btn btn-primary custom">Search</button>
         </div>
-        <section>
+        <section class="listing">
+            <div class="container featured">
+                    <h2 class="text-center">Featured Businesses</h2>
+                    <div class="row justify-content-md-center">
+                        <div class="col-md-4 custom">
+                            <div class="card" style="width: 14rem;">
+                                <img class="card-img-top" src="../images/download.svg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">The Office</h5>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star"></span>
+                                        <span class="fa fa-star"></span>
+                                        <span class="review-small"> 21 Reviews</span>
+                                        <p class="card-text">Resturant, Bar, Lounge</p>
+                                        <p class="card-text"><span class="fas fa-map-marker-alt"></span> GRA</p>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 custom">
+                                <div class="card" style="width: 14rem;">
+                                    <img class="card-img-top" src="../images/download.svg" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">The Office</h5>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star checked"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="fa fa-star"></span>
+                                            <span class="review-small"> 21 Reviews</span>
+                                            <p class="card-text">Resturant, Bar, Lounge</p>
+                                            <p class="card-text"><span class="fas fa-map-marker-alt"></span> GRA</p>
+                                        </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 custom">
+                                    <div class="card" style="width: 14rem;">
+                                        <img class="card-img-top" src="../images/download.svg" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">The Office</h5>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="review-small"> 21 Reviews</span>
+                                                <p class="card-text">Resturant, Bar, Lounge</p>
+                                                <p class="card-text"><span class="fas fa-map-marker-alt"></span> GRA</p>
+                                            </div>
+                                    </div>
+                                </div>
+                </div>
+            </div>
             <div class="container">
-                <div class="col-md-12">
-
+                    <h2 class="text-center hr"></h2>
+                    <div class="row justify-content-md-center">
+                            <div class="col-md-4 custom">
+                                    <div class="card" style="width: 14rem;">
+                                        <img class="card-img-top" src="../images/download.svg" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">The Office</h5>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="review-small"> 21 Reviews</span>
+                                                <p class="card-text">Resturant, Bar, Lounge</p>
+                                                <p class="card-text"><span class="fas fa-map-marker-alt"></span> GRA</p>
+                                            </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 custom">
+                                        <div class="card" style="width: 14rem;">
+                                            <img class="card-img-top" src="../images/download.svg" alt="Card image cap">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">The Office</h5>
+                                                    <span class="fa fa-star checked"></span>
+                                                    <span class="fa fa-star checked"></span>
+                                                    <span class="fa fa-star checked"></span>
+                                                    <span class="fa fa-star"></span>
+                                                    <span class="fa fa-star"></span>
+                                                    <span class="review-small"> 21 Reviews</span>
+                                                    <p class="card-text">Resturant, Bar, Lounge</p>
+                                                    <p class="card-text"><span class="fas fa-map-marker-alt"></span> GRA</p>
+                                                </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 custom">
+                                            <div class="card" style="width: 14rem;">
+                                                <img class="card-img-top" src="../images/download.svg" alt="Card image cap">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">The Office</h5>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star checked"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="fa fa-star"></span>
+                                                        <span class="review-small"> 21 Reviews</span>
+                                                        <p class="card-text">Resturant, Bar, Lounge</p>
+                                                        <p class="card-text"><span class="fas fa-map-marker-alt"></span> GRA</p>
+                                                    </div>
+                                            </div>
+                                        </div>
                 </div>
             </div>
         </section>
