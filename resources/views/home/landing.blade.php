daf0d2dc2e1394e1b24a1bb8fbb70226b5bdda9b
Ikechukwu Nwakanma <nwakanma.ikechukwu@yahoo.com>
2018-11-17 21:33:34 +0100

    update landing page

---
+++ resources/views/home/landing.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <main>
+        <div class="jumbotron custom text-center">
+            <h1>FIND NICE SPOTS AROUND YOU</h1>
+            <input type="text" name="" placeholder="Resturants, Bars, Location" class="form-control custom" id=""><button class="btn btn-primary custom">Search</button>
+        </div>
+        <section>
+            <div class="container">
+                <div class="col-md-12">
+
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+                <div class="text-5xl">ike</div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
